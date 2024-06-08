a652acb2c485abd19d3d35e28ee0a9f0ffd19c52
Sridhar D Kedlaya <sridhardked@gmail.com>
2024-06-08 08:14:21 +0530

    Add frontend

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, make_response
 from flask_jwt_extended import JWTManager
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import jwt_required, get_jwt_identity
@@ -11,7 +11,9 @@ jwt = JWTManager(app)
 @app.route("/", methods=["GET"])
 def login():
     access_token = create_access_token(identity=0)
-    return jsonify(access_token=access_token), 200
+    response = make_response(render_template('index.html', token=access_token))
+    response.headers['Authorization'] = f'Bearer {access_token}'
+    return response
 
 
 @app.route("/protected", methods=["GET"])
@@ -19,5 +21,8 @@ def login():
 def protected():
     current_user_id = get_jwt_identity()
     if current_user_id == 1:
-        return jsonify({"flag": "<flag_text>"}), 200
-    return jsonify({"flag": "Unauthorised"}), 200
+        response = make_response(render_template('protected.html'))
+        return response
+    else:
+        response = make_response(render_template('impaler.html'))
+        return response
